b84ee4db5f08527367d188d2994849ec1840207b
Dave McKeown <davecmckeown@gmail.com>
2012-12-19 11:53:07 -0500

    a few fixes so that it builds with M4A 4.2.7

--- MonoDroid.Dialog/DialogHelper.cs
+++ MonoDroid.Dialog/DialogHelper.cs
@@ -22,23 +22,24 @@ namespace MonoDroid.Dialog
             this.Root.Context = context;
 
             dialogView.Adapter = this.DialogAdapter = new DialogAdapter(context, this.Root);
-            dialogView.ItemClick += new EventHandler<ItemEventArgs>(ListView_ItemClick);
+            dialogView.ItemClick += new EventHandler<AdapterView.ItemClickEventArgs>(dialogView_ItemClick);
             //dialogView.ItemLongClick += new EventHandler<ItemEventArgs>(ListView_ItemLongClick);
             dialogView.Tag = root;
         }
 
-        void ListView_ItemLongClick(object sender, ItemEventArgs e)
-        {
-            var elem = this.DialogAdapter.ElementAtIndex(e.Position);
-            if (elem != null && elem.LongClick != null)
-                elem.LongClick(sender, e);
-        }
-
-        void ListView_ItemClick(object sender, ItemEventArgs e)
+        void dialogView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
             var elem = this.DialogAdapter.ElementAtIndex(e.Position);
             if (elem != null && elem.Click != null)
                 elem.Click(sender, e);
         }
+
+        //void ListView_ItemLongClick(object sender, ItemEventArgs e)
+        //{
+        //    var elem = this.DialogAdapter.ElementAtIndex(e.Position);
+        //    if (elem != null && elem.LongClick != null)
+        //        elem.LongClick(sender, e);
+        //}
+
     }
 }
